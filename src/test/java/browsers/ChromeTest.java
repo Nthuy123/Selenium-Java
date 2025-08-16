76349f4869dbd344ac53be9f3319852e82bc7948
Nthuy123 <nhatthuy200601@gmail.com>
2025-08-16 13:14:04 +0700

    add mobile view port test casse

--- src/test/java/browsers/ChromeTest.java
+++ src/test/java/browsers/ChromeTest.java
@@ -6,15 +6,18 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ChromeTest {
-    /*@Test
+    @Test
     public void normalModeTest(){
         WebDriver driver = new ChromeDriver();
         driver.get("https:/www.selenium.dev/");
         Assert.assertEquals(driver.getTitle(),"Selenium");
         driver.quit();
 
-    }*/
+    }
 
     @Test
     public void headlessModeTest() {
@@ -26,5 +29,20 @@ public class ChromeTest {
         Assert.assertEquals(driver.getTitle(),"Selenium");
         driver.quit();
     }
+    @Test
+    void MobileViewPortraitTest(){
+        Map<String, Object> deviceMetrics = new HashMap<>();
+        deviceMetrics.put("width", 344);
+        deviceMetrics.put("height", 882);
+        Map<String, Object> mobileEmulation = new HashMap<>();
+        mobileEmulation.put("deviceMetrics", deviceMetrics);
 
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://www.selenium.dev/");
+        Assert.assertEquals(driver.getTitle(),"Selenium");
+        driver.quit();
+    }
 }
